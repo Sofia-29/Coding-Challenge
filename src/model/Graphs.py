c4085fd83968459663756a2dd3e091d5c6274f90
Sofia-29 <angiesofiag04@gmail.com>
2022-08-10 22:30:57 -0600

    Add comments to model classes

--- src/model/Graphs.py
+++ src/model/Graphs.py
@@ -7,8 +7,20 @@ class Graphs:
 
     def __init__(self) -> None:
         pass
-    
+
     def setup_plot(self, x_axis_data:list, y_axis_data, title:str, x_label:str, y_label:str):
+        '''
+            Setup scatterplot graph for showing data
+
+            PARAMS
+
+            x_axis_data:list, list containing x axis data
+            y_axis_data, list containing y axis data
+            title:str, title of graph
+            x_label:str, x label of graph
+            y_label:str, y label of graph
+
+        '''
         plt.scatter(x_axis_data, y_axis_data, color = 'black')
         plt.plot(x_axis_data, y_axis_data, color = "green")
         plt.title(title)
@@ -16,14 +28,29 @@ class Graphs:
         plt.ylabel(y_label)
     
     def show_plot(self):
+        '''
+            Shows scatterplot graph previously set
+        '''
         plt.show()
 
     def setup_heatmap(self, matrix:list, title:str):
+        '''
+            Setup heatmap for showing data
+
+            PARAMS
+
+            x_axis_data:list, list containing x axis data
+            y_axis_data, list containing y axis data
+            title:str, title of graph
+        '''
         matrix_display = ConfusionMatrixDisplay(confusion_matrix = matrix, display_labels = [False, True])
         matrix_display.plot(cmap='Greens')
         plt.title(title)
 
     def get_heatmap(self):
+        '''
+            Shows heatmap graph previously set
+        '''
         plt.show()
     
     
