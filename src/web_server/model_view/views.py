ca38c3f04b755480f7acec597b4316fc0e89273e
Sofia-29 <angiesofiag04@gmail.com>
2022-08-13 18:56:57 -0600

    Add final version

--- src/web_server/model_view/views.py
+++ src/web_server/model_view/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 from .forms import DataForm
